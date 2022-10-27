6335a132689e70716a4b56134740c2eb16c95969
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-10-28 00:08:51 +0100

    added Zuri Team and Zuri Books using the Link component

--- linktree-profile-page/src/components/ExternalLinks.js
+++ linktree-profile-page/src/components/ExternalLinks.js
@@ -4,7 +4,10 @@ const ExternalLinks = () => {
     return (
         <section>
             <Link id="twitter" url="https://twitter.com/db_ezekiel" title="@db_ezekiel"/>
-           <Link id="slack" url="https://hng9.slack.com/" title="@db-ezekiel" />
+            <Link id="slack" url="https://hng9.slack.com/" title="@db-ezekiel" />
+
+            <Link id="btn_zuri" url="https://training.zuri.team/" title="Zuri Team" />
+            <Link id="books" url="https://books.zuri.team" />
         </section>
     )
 }
