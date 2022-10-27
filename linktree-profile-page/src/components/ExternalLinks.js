b8ed6265844a0d426685f00a102e79af4752be3d
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-10-28 00:06:59 +0100

    added slack username to ExternalLinks component

--- linktree-profile-page/src/components/ExternalLinks.js
+++ linktree-profile-page/src/components/ExternalLinks.js
@@ -3,7 +3,8 @@ import Link from './Link';
 const ExternalLinks = () => {
     return (
         <section>
-            <Link id="twitter" url="https://twitter.com/db_ezekiel" title="Twitter Link"/>
+            <Link id="twitter" url="https://twitter.com/db_ezekiel" title="@db_ezekiel"/>
+           <Link id="slack" url="https://hng9.slack.com/" title="@db-ezekiel" />
         </section>
     )
 }
