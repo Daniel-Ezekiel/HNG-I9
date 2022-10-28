0b08ad12c7ff80e38d3551032b87cc722cc14d79
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-10-28 01:16:35 +0100

    added share buttons to the profile section

--- linktree-profile-page/src/components/ProfileInfo.js
+++ linktree-profile-page/src/components/ProfileInfo.js
@@ -3,6 +3,9 @@ import Image from "./Image";
 const ProfileInfo = ({elementId}) => {
     return (
         <section id={elementId}>
+            <img className="share-button" src="./img/share_mobile.svg" />
+            <img className="icon-button" src="./img/share_desktop.svg" />
+
             <Image id="profile__img" imgUrl="https://danielezekiel.me/img/profile.webp" alt="Daniel Ezekiel" />
 
             <h1>Daniel Ezekiel</h1>
