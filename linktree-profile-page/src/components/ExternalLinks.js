427529cbce6e9939b191b23acc3ef646f18a0cd9
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-11-03 14:28:53 +0100

    feat: added a button to route to the contact page

--- linktree-profile-page/src/components/ExternalLinks.js
+++ linktree-profile-page/src/components/ExternalLinks.js
@@ -12,6 +12,7 @@ const ExternalLinks = () => {
             <Link id="book__python" url="https://books.zuri.team/python-for-beginners?ref_id=danielezekiel/" name="Python Books" title="The Complete Python Package - Learn Python without stress." />
             <Link id="pitch" url="https://background.zuri.team/" name="Background Check for Coders" title="Exquisite and Premium Background Check Services for Coders" />
             <Link id="book__design" url="https://books.zuri.team/design-rules" name="Design Books" title="The All-In-One Design Rules Book by the Zuri Team" />
+            <Link id="contact" url="/contact" name="Contact"/>
 
             <Socials />
         </section>
