436fe7d5799c4201cdc8806f3e286ab9d9bb4faf
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-11-03 14:28:02 +0100

    feat: created contact component with form embedded

---
+++ linktree-profile-page/src/components/Contact.js
@@ -0,0 +1,16 @@
+import Form from './Form'
+
+const Contact = () => {
+    return (
+        <main>
+            <div clasName="top-section">
+                <h1>Contact Me</h1>
+                <p>Hi there, contact me to ask me about anything you have in mind</p>
+            </div>
+
+            <Form />
+        </main>
+    );
+}
+
+export default Contact;
