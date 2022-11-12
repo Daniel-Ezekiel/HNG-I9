4b204813a6ae9f5372d6d871a7ec33a6614e2f07
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-11-12 10:05:29 +0100

    feat: added route to placetostay page

--- metabnb/src/App.js
+++ metabnb/src/App.js
@@ -2,6 +2,7 @@ import { Homepage } from './pages/Homepage';
 import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import PlaceToStay from './pages/PlaceToStay';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
       <Routes>
         <Route path='/' element={ <Homepage /> } />
+        <Route path='/placetostay' element={ <PlaceToStay /> } />
       </Routes>
 
       <Footer />
