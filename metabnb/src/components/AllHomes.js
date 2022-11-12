764149570363d1c212025bad38c2341eb7194641
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-11-12 06:55:54 +0100

    feat: created AllHomes component to improve reusability in other pages

---
+++ metabnb/src/components/AllHomes.js
@@ -0,0 +1,89 @@
+import Home from "./Home"
+
+const AllHomes = () => {
+  return (
+    <div className="all-homes">
+        <div className="homes-container">
+                <Home cardName="card-img1"
+                      homeName="Desert king"
+                      homePrice="1MBT"
+                      homeDistance="2345km" 
+                      homeAvailability="available for 2weeks stay"
+                      homeRating='5 stars' 
+                />
+            </div>
+
+            <div className="homes-container">
+                <Home cardName="card-img2"
+                      homeName="Desert king"
+                      homePrice="1MBT"
+                      homeDistance="2345km" 
+                      homeAvailability="available for 2weeks stay"
+                      homeRating='5 stars' 
+                />
+            </div>
+
+            <div className="homes-container">
+                <Home cardName="card-img3"
+                      homeName="Desert king"
+                      homePrice="1MBT"
+                      homeDistance="2345km" 
+                      homeAvailability="available for 2weeks stay"
+                      homeRating='5 stars' 
+                />
+            </div>
+
+            <div className="homes-container">
+                <Home cardName="card-img4"
+                      homeName="Desert king"
+                      homePrice="1MBT"
+                      homeDistance="2345km" 
+                      homeAvailability="available for 2weeks stay"
+                      homeRating='5 stars' 
+                />
+            </div>
+
+            <div className="homes-container">
+                <Home cardName="card-img5"
+                      homeName="Desert king"
+                      homePrice="1MBT"
+                      homeDistance="2345km" 
+                      homeAvailability="available for 2weeks stay"
+                      homeRating='5 stars' 
+                />
+            </div>
+
+            <div className="homes-container">
+                <Home cardName="card-img6"
+                      homeName="Desert king"
+                      homePrice="1MBT"
+                      homeDistance="2345km" 
+                      homeAvailability="available for 2weeks stay"
+                      homeRating='5 stars' 
+                />
+            </div>
+
+            <div className="homes-container">
+                <Home cardName="card-img7"
+                      homeName="Desert king"
+                      homePrice="1MBT"
+                      homeDistance="2345km" 
+                      homeAvailability="available for 2weeks stay"
+                      homeRating='5 stars' 
+                />
+            </div>
+
+            <div className="homes-container">
+                <Home cardName="card-img8"
+                      homeName="Desert king"
+                      homePrice="1MBT"
+                      homeDistance="2345km" 
+                      homeAvailability="available for 2weeks stay"
+                      homeRating='5 stars' 
+                />
+            </div>
+    </div>
+  )
+}
+
+export default AllHomes
