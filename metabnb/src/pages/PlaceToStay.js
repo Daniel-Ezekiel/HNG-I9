dadc0d5b664b0f902009bbc54bf7ce520ec3da48
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-11-12 10:31:34 +0100

    feat: created placetostay page and added its dependent components

---
+++ metabnb/src/pages/PlaceToStay.js
@@ -0,0 +1,23 @@
+import AllHomes from "../components/AllHomes";
+import Search from "../components/Search";
+import { Link } from "react-router-dom";
+import '../placetostay.css'
+
+export const PlaceToStay = () => {
+    const categories = ['Restaurant', 'Cottage', 'Castle', 'Fantast City', 'Beach', 'Cabin', 'Off-Grid', 'Farm'];
+
+    return (
+        <section id="all-homes-section">
+            <div className="location-categories">
+                <ul style={ { justifyContent: 'space-evenly', flexFlow: 'row wrap'} }>
+                    {categories.map( (location, id) => <li key={id}><Link to="#">{location}</Link></li> )}
+                </ul>
+
+                <Search className="location-search" placeholder="Location" btnName={<img src="img/filter.png" alt="icon" />}/>
+            </div>
+            <AllHomes />
+        </section>
+    );
+}
+
+export default PlaceToStay;
