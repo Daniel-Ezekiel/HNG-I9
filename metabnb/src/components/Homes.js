f7a724b4e73f869654302b5a5a25b43a0f161a81
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-11-12 06:56:32 +0100

    feat: added AllHomes component to Homes component, to display all homes in homepage

--- metabnb/src/components/Homes.js
+++ metabnb/src/components/Homes.js
@@ -1,89 +1,11 @@
-import Home from "./Home";
+import AllHomes from "./AllHomes";
 
 const Homes = () => {
     return (
         <section id="homes-section">
             <h2>Inspiration for your next adventure</h2>
 
-            <div className="homes-container">
-                <Home cardName="card-img1"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating='5 stars' 
-                />
-            </div>
-
-            <div className="homes-container">
-                <Home cardName="card-img2"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating='5 stars' 
-                />
-            </div>
-
-            <div className="homes-container">
-                <Home cardName="card-img3"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating='5 stars' 
-                />
-            </div>
-
-            <div className="homes-container">
-                <Home cardName="card-img4"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating='5 stars' 
-                />
-            </div>
-
-            <div className="homes-container">
-                <Home cardName="card-img5"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating='5 stars' 
-                />
-            </div>
-
-            <div className="homes-container">
-                <Home cardName="card-img6"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating='5 stars' 
-                />
-            </div>
-
-            <div className="homes-container">
-                <Home cardName="card-img7"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating='5 stars' 
-                />
-            </div>
-
-            <div className="homes-container">
-                <Home cardName="card-img8"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating='5 stars' 
-                />
-            </div>
+            <AllHomes />
         </section>
     )
 }
