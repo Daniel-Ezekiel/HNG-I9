2a74fb5326e7f1c09a89308ce039d29388856ae5
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-11-12 18:25:33 +0100

    feat: added modal component to the website

--- metabnb/src/pages/Homepage.js
+++ metabnb/src/pages/Homepage.js
@@ -2,11 +2,14 @@ import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import Homes from '../components/Homes'
 import Nft from '../components/Nft';
+import Modal from '../components/Modal';
 
 export const Homepage = () => {
+
     return (
         <main>
             <Hero />
+            <Modal/>
             <Banner />
             <Homes />
             <Nft />
