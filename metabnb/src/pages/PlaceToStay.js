4db4496bd3cc9b08e6b9f849ff5ffb258c2486d7
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-11-13 13:56:29 +0100

    feat: added all homes to the placetostay page to match figma design

--- metabnb/src/pages/PlaceToStay.js
+++ metabnb/src/pages/PlaceToStay.js
@@ -6,6 +6,8 @@ import '../placetostay.css'
 export const PlaceToStay = () => {
     const categories = ['Restaurant', 'Cottage', 'Castle', 'Fantast City', 'Beach', 'Cabin', 'Off-Grid', 'Farm'];
 
+    const homeImgs = ['card-img5', 'card-img6', 'card-img7', 'card-img8', 'card-img1', 'card-img2', 'card-img3', 'card-img4', 'card-img9', 'card-img10', 'card-img11', 'card-img12', 'card-img13', 'card-img14', 'card-img15', 'card-img16']
+
     return (
         <section id="all-homes-section">
             <div className="location-categories">
@@ -15,7 +17,7 @@ export const PlaceToStay = () => {
 
                 <Search className="location-search" placeholder="Location" btnName={<img src="img/filter.png" alt="icon" />}/>
             </div>
-            <AllHomes />
+            <AllHomes homeImgs={homeImgs}/>
         </section>
     );
 }
