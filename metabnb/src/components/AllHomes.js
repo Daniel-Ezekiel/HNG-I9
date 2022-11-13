456f82c06557becc583636bc18a0423e14856a97
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-11-13 13:55:18 +0100

    refactor: made the allhomes component able to dynamically populate the home cards

--- metabnb/src/components/AllHomes.js
+++ metabnb/src/components/AllHomes.js
@@ -1,90 +1,25 @@
 import Home from "./Home";
 import { FaStar } from 'react-icons/fa';
 
-const AllHomes = () => {
+const AllHomes = ( {homeImgs} ) => {
     const rating = <><FaStar/><FaStar/><FaStar/><FaStar/><FaStar/></>
 
   return (
     <div className="all-homes">
-        <div className="homes-container">
-                <Home cardName="card-img1"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating={rating} 
-                />
-            </div>
-
-            <div className="homes-container">
-                <Home cardName="card-img2"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating={rating} 
-                />
-            </div>
-
-            <div className="homes-container">
-                <Home cardName="card-img3"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating={rating} 
-                />
-            </div>
-
-            <div className="homes-container">
-                <Home cardName="card-img4"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating={rating} 
-                />
-            </div>
-
-            <div className="homes-container">
-                <Home cardName="card-img5"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating={rating} 
-                />
-            </div>
-
-            <div className="homes-container">
-                <Home cardName="card-img6"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating={rating} 
-                />
-            </div>
-
-            <div className="homes-container">
-                <Home cardName="card-img7"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating={rating} 
-                />
-            </div>
-
-            <div className="homes-container">
-                <Home cardName="card-img8"
-                      homeName="Desert king"
-                      homePrice="1MBT"
-                      homeDistance="2345km" 
-                      homeAvailability="available for 2weeks stay"
-                      homeRating={rating} 
-                />
-            </div>
+        {homeImgs.map( (item, id) => {
+                return (
+                    <div className="homes-container">
+                        <Home key={id} cardName={item}
+                                            homeName="Desert king"
+                                            homePrice="1MBT"
+                                            homeDistance="2345km" 
+                                            homeAvailability="available for 2weeks stay"
+                                            homeRating={rating} 
+                                        />
+                    </div>
+                );
+            } 
+        )}
     </div>
   )
 }
--- metabnb/src/components/Homes.js
+++ metabnb/src/components/Homes.js
@@ -1,12 +1,14 @@
 import AllHomes from "./AllHomes";
 
 const Homes = () => {
+    const homeImgs = ['card-img1', 'card-img2', 'card-img3', 'card-img4', 'card-img5', 'card-img6', 'card-img7', 'card-img8'];
+
     return (
         <section id="homes-section">
             <div>
                 <h2>Inspiration for your next adventure</h2>
 
-                <AllHomes />
+                <AllHomes homeImgs={homeImgs}/>
             </div>
         </section>
     )
